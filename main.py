fbc4702243523e7da6394c81b95b9a03164e7175
usercspa <>
2021-04-25 03:33:51 +0000

    game loop with temporary human image

--- main.py
+++ main.py
@@ -37,8 +37,27 @@ def player(x,y):
 def game_opening():
     screen.blit(opening,(0,0))
     pygame.display.update()
-    time.sleep(1)    
+    time.sleep(2)    
 
+#end screen
+def game_ending():
+    pygame.time.delay(500)
+    screen.blit(crashed,(0,0))
+    pygame.display.update()
+    pygame.time.delay(3000)
+    
+  
+#obstacles
+def obstacles(obx, oby, obw, obh, color):
+    pygame.draw.rect(screen, color, [obx, oby, obw, obh])     
+
+#score
+def obstacle_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    screen.blit(text,(350,50))    
+
+     
 #game loop    
 def game_loop():
 
@@ -49,10 +68,68 @@ def game_loop():
     y = (HEIGHT * 0.8)
 
     x_change = 0
-      
-    pygame.display.update()
-    clock.tick(60)
+
+    ob_startx = random.randrange(0, WIDTH)
+    ob_starty = -600
+    ob_speed = 7
+    ob_width = 100
+    ob_height = 100
+
+    obCount = 1
+
+    dodged = 0
+
+    gameExit = False
+
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        screen.fill(grey)
+
+        obstacles(ob_startx, ob_starty, ob_width, ob_height, white)
+        ob_starty += ob_speed
+        player(x,y)
+        obstacle_dodged(dodged)
+
+        if x > WIDTH - player_width or x < 0:
+            game_ending()
+
+        if ob_starty > HEIGHT:
+            ob_starty = 0 - ob_height
+            ob_startx = random.randrange(0,WIDTH)
+            dodged += 1
+            ob_speed += 1
+            ob_width += (dodged * 1.2)
+
+
+        if y < ob_starty+ob_height:
+            
+            if x > ob_startx and x < ob_startx + ob_width or x+player_width > ob_startx and x + player_width < ob_startx+ob_width:
+                
+          
+              game_ending()
+
+        
+        pygame.display.update()
+        clock.tick(60)
 
 
 game_loop()
+pygame.quit()
+quit()
  
