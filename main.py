8053399e653a0f9025c2baa91c6dd087378df302
usercspa <>
2021-04-25 01:24:20 +0000

    initial code

---
+++ main.py
@@ -0,0 +1,24 @@
+#install packages
+import pygame
+import time
+import random
+
+#initialize
+pygame.init()
+
+#screen
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Cvoid stimulation game")
+clock = pygame.time.Clock()
+
+#colors
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+grey = (123,123,123)
+
+player_width = 56
+
